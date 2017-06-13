6061de82d8db07dababa6ae67c768e58b8fd7d87
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-06-13 15:14:28 +0530

    Add files via upload

---
+++ fc17.java
@@ -0,0 +1,12 @@
+class fc17{
+public static void main(String[] args){
+int a=Integer.parseInt(args[0]);
+int rev=0;
+while(a!=0){
+rev=rev*10;
+rev=rev+a%10;
+a=a/10;
+}
+System.out.println("reverse no is:"+rev);
+}
+}
