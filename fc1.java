fb973c151f93ed733f29857ef17af83be0a43473
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-06-13 09:33:25 +0530

    Add files via upload

---
+++ fc1.java
@@ -0,0 +1,11 @@
+class fc1{
+public static void main(String[] args){
+int a=Integer.parseInt(args[0]);
+if(a>0){
+System.out.println("the no is positive");}
+else if(a==0){
+System.out.println("the no is zero");}
+else{
+System.out.println("the no is negative");}
+}
+}
