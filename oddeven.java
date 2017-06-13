74df5f553361b8fbd1dcabb2e79566c38076532e
Ibrahim5697 <ibrahim.jece@ksrce.ac.in>
2017-06-13 09:41:32 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,9 @@
+class evenodd{
+public static void main(String[] args){
+int a=Integer.parseInt(args[0]);
+if(a%2==0)
+System.out.println("the no is even");
+else 
+System.out.println("the no is odd");
+}
+}
